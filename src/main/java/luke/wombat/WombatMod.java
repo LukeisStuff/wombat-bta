82d3727f5506c178c2a5003fb8f7a66a75862a90
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-01 12:12:33 -0400

    Removed example mod stuff implement dragonfly

--- src/main/java/luke/wombat/WombatMod.java
+++ src/main/java/luke/wombat/WombatMod.java
@@ -1,19 +1,18 @@
-package turniplabs.examplemod;
+package luke.wombat;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class WombatMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+    public static final String MOD_ID = "wombat";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("Wild Wombats initialized.");
     }
 
 	@Override
@@ -28,6 +27,11 @@ public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEn
 
 	@Override
 	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
 
 	}
 }
