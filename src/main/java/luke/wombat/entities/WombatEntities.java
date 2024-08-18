5dc5ac713febee6aefd7db68a9f32c1fa08406e1
LukeisStuff <lukeisawesome03@gmail.com>
2024-08-18 15:45:49 -0400

    Updated to 7.2_01

--- src/main/java/luke/wombat/entities/WombatEntities.java
+++ src/main/java/luke/wombat/entities/WombatEntities.java
@@ -3,7 +3,7 @@ package luke.wombat.entities;
 import turniplabs.halplibe.helper.EntityHelper;
 
 public class WombatEntities {
-	private static int entityID = 250;
+	private static int entityID = 230;
 
 	public void initializeEntities() {
 		EntityHelper.createEntity(EntityWombat.class, entityID++, "Wombat", () -> new WombatRenderer(new ModelWombat(), new ModelWombat(0.5f), 0.7f));
