f0938038c4733f258d63c00f92eefc1eba54907f
LukeisStuff <lukeisawesome03@gmail.com>
2025-03-21 03:37:41 -0400

    Update version name and add a new variant, and now activator can play the didgeridoo

--- src/main/java/luke/wombat/items/ItemDidgeridoo.java
+++ src/main/java/luke/wombat/items/ItemDidgeridoo.java
@@ -4,6 +4,7 @@ import net.minecraft.core.block.entity.TileEntityActivator;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.sound.SoundCategory;
 import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.world.World;
 
@@ -20,6 +21,6 @@ public class ItemDidgeridoo extends Item {
 	}
 
 	public void onUseByActivator(ItemStack itemStack, TileEntityActivator activatorBlock, World world, Random random, int blockX, int blockY, int blockZ, double offX, double offY, double offZ, Direction direction) {
-		world.playSoundAtEntity(null, null, "ambient.cave.cave", 1.0F, 0.5f);
+		world.playSoundEffect(null, SoundCategory.CAVE_SOUNDS, blockX, blockY, blockZ, "ambient.cave.cave", 1.0F, 0.5f);
 	}
 }
